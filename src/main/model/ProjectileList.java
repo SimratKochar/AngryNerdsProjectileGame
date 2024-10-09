15e47f72bf8fbe16a19eadee7f740d0b185c32a9
sskochar <sskochar@student.ubc.ca>
2024-10-09 14:43:26 -0700

    Added Implementation for ProjectileList

--- src/main/model/ProjectileList.java
+++ src/main/model/ProjectileList.java
@@ -18,17 +18,29 @@ public class ProjectileList {
     // MODIFIES: this
     // EFFECTS: adds a projectile to the list of projectiles
     public void addProjectile(Projectile projectile) {
-        // stub
+        projectiles.add(projectile);
     }
 
     // EFFECTS: creates a list of velocities of projectiles fired until now
     public List<Integer> listVelocities() {
-        return null;   // stub
+        ArrayList<Integer> velocities = new ArrayList<Integer>();
+        
+        for (Projectile projectile: projectiles) {
+            velocities.add(projectile.getVelocity());
+        }
+        
+        return velocities;
     }
 
     // EFFECTS: creates a list of angles of projectiles fired until now
     public List<Integer> listAngles() {
-        return null;   // stub
+        ArrayList<Integer> angles = new ArrayList<Integer>();
+        
+        for (Projectile projectile: projectiles) {
+            angles.add(projectile.getAngle());
+        }    
+
+        return angles;
     }
 
 
@@ -38,12 +50,12 @@ public class ProjectileList {
 
     // EFFECTS: returns the list of projectiles
     public List<Projectile> getProjectiles() {
-        return null; // stub
+        return projectiles; // stub
     }
 
     // EFFECTS: returns the number of projectiles in list to represent attempts
     public int getNumAttempts() {
-        return 0; // stub
+        return projectiles.size(); // stub
     }
 
 }
