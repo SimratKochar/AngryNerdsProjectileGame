69a90b05fffbf8c5962e77a785c3c5f1e83286d9
sskochar <sskochar@student.ubc.ca>
2024-10-09 16:57:51 -0700

    Implemented the game console ui

---
+++ src/main/ui/GameConsole.java
@@ -0,0 +1,70 @@
+package ui;
+
+import java.util.Scanner;
+
+import model.Projectile;
+import model.ProjectileList;
+import model.Target;
+
+/*
+ * Projectile Motion Game Console application
+ */
+public class GameConsole {
+    private Projectile projectile;
+    private ProjectileList projList;
+    private Target target;
+
+    private Scanner input;
+
+    // EFFECTS: runs the game
+    public GameConsole() {
+        projList = new ProjectileList();
+        target = new Target();
+        runGame();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    public void runGame() {
+        input = new Scanner(System.in);
+        projectile = new Projectile(1, 1);
+
+        System.out.println("The target is at position (" + target.getX() + 
+                ", " + target.getY() + "). Use kinematics equations to find " + 
+                "the initial velocity and angle required to hit the target!");
+
+        while (true) {
+            System.out.println("Previously entered velocities: " + projList.listVelocities());
+            System.out.println("Previously entered angles: " + projList.listAngles());
+            System.out.println("Enter velocity (m/s): ");
+            int velocity = input.nextInt();
+            System.out.println("Enter angle (degrees): ");
+            int angle = input.nextInt();
+
+            projectile = new Projectile(velocity, angle);
+            projList.addProjectile(projectile);
+
+            System.out.println("Maximum height reached: " + projectile.maxHeight() + " meters");
+            System.out.println("Approximate flight time: " + projectile.flightTime() + " seconds");
+
+            moveProjectile();
+
+        }
+    }
+
+    public void moveProjectile() {
+        double time = 0.1;
+        while (!projectile.groundHit()) {
+            projectile.updateCoordinates(0);
+            time += 0.1;
+
+            if (projectile.targetHit(target)) {
+                System.out.println("Congratulations, you hit the target!");
+                break;
+            }
+        }
+        System.out.println("You missed the target, try again!");
+
+    }
+
+}
