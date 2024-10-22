140f4835870f8068ab8d8ae02175099c8530b93c
sskochar <sskochar@student.ubc.ca>
2024-10-22 16:33:01 -0700

    Added tests for JsonReader

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,60 @@
+package persistence;
+
+import org.junit.jupiter.api.Test;
+
+import model.Projectile;
+import model.ProjectileList;
+import model.Target;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Referenced from https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
+public class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/NonExistentFile.json");
+
+        try {
+            Target t = reader.readTarget();
+            ProjectileList pl = reader.readPList();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // expected
+        }
+    }
+
+    @Test
+    void testReaderEmptyProjectileList() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyProjectileList.json");
+        try {
+            Target t = reader.readTarget();
+            checkTarget(88, 157, t);
+
+            ProjectileList pl = reader.readPList();
+            assertEquals(0, pl.getNumAttempts());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+   }
+
+   @Test
+   void testGeneralGameProgress() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralGameProgress");
+        try {
+            Target t = reader.readTarget();
+            checkTarget(45, 80, t);
+
+            ProjectileList pl = reader.readPList();
+            List<Projectile> projectiles = pl.getProjectiles();
+            assertEquals(2, pl.getNumAttempts());
+            checkProjectile(50, 30, projectiles.get(0));
+            checkProjectile(50, 20, projectiles.get(1));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+   }
+}
