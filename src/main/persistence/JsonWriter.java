48f0b9e153ed0c5feb0e4312b63160fff16ac7e6
sskochar <sskochar@student.ubc.ca>
2024-10-22 16:40:48 -0700

    Added implementation for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,7 @@
 package persistence;
 
+import org.json.JSONObject;
+
 import model.ProjectileList;
 import model.Target;
 
@@ -8,34 +10,40 @@ import java.io.*;
 // Referenced from https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
 // Represents a writer that writes JSON representation of game progress to file
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        // stub
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of Target and ProjectileList to file
     public void write(Target t, ProjectileList pl) {
-        // stub
+        JSONObject json = pl.toJson();
+        json.put("Target", t.toJson());
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
 }
+
